9397d988da6c0cebad23a4ccaf11feb147010813
Adrián Hojas Moré <hojas_adrian@MacBook-Air-de-Adrian.local>
2025-05-16 10:19:41 -0400

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,320 @@
+function Hero() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="nav">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">work</a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./eye.png" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="titlegrapper">
+          <p className="addres">
+            Havana, cuba
+            <br />
+            <a href="">jjmontesinodesign@gmail.com</a>
+          </p>
+          <h1>
+            JULIO j.
+            <br />
+            MONTESINO
+          </h1>
+          <div className="container">
+            <img src="./eye.png" alt="" />
+          </div>
+          <h2>
+            Illustrator,
+            <br />
+            designer & animator
+          </h2>
+          <p className="addres clients">
+            Clients Include
+            <br />
+            <a href="">
+              Apple, Amazon, Adobe, The New York Times, ShakeShack, Lululemon,
+              NPR
+            </a>
+          </p>
+        </div>
+      </header>
+      <section>
+        <h2>
+          Reserved, <br />
+          But Far From
+          <br />
+          Ordinary!
+        </h2>
+        <p>
+          Hailing from Canada and thriving in the heart of LA, I’ve spent the
+          last 7 years partnering with visionary founders and VC- backed
+          startups to craft brands, websites, apps, and marketing that leave a
+          lasting impression.
+        </p>
+        <p>
+          My mission? To transform big ideas into compelling digital experiences
+          that captivate users, elevate products, and catch the eyes of
+          investors. Together, we don’t just tell stories – we create moments
+          that make your brand unforgettable.
+        </p>
+      </section>
+
+      <section className="pills">
+        <ul>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+        </ul>
+        <ul>
+          <li>Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+        </ul>
+        <ul>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+          <li>Logo Design</li>
+        </ul>
+      </section>
+      <section className="work">
+        <div className="title-wraper">
+          <h2>
+            Featured
+            <br />
+            Work
+          </h2>
+          <p>Selected and recent notable projects</p>
+        </div>
+        <ul>
+          <li>
+            <img src="./work/photo_5849132338141837449_y.jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5849542181101090869_w.jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5849298295678155231_y.jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5849375815542880712_w (1).jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5849563363879795274_y.jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5851541479787444253_y.jpg" alt="" />
+          </li>
+          <li>
+            <img src="./work/photo_5849132338141837449_y.jpg" alt="" />
+          </li>
+        </ul>
+        <button>Get started for free</button>
+      </section>
+
+      <section className="true">
+        <div className="imagecard">
+          <img
+            className="imgcard"
+            src="./work/photo_5849595812357716054_w (1).jpg"
+            alt=""
+          />
+          <img
+            className="imgcard"
+            src="./work/photo_5849595812357716054_w (1).jpg"
+            alt=""
+          />
+          <img
+            className="imgcard"
+            src="./work/photo_5851760475874899803_w (4).jpg"
+            alt=""
+          />
+        </div>
+        <div className="info">
+          <div className="titlegrapper">
+            <p className="addres">Why Headspace</p>
+            <h2>Comprehensive care for everyone</h2>
+          </div>
+          <div className="faqcard">
+            <details name="faqs" open>
+              <summary>
+                <div className="title">
+                  <span>01</span> Full spectrum care for a global team
+                </div>
+              </summary>
+              <p>
+                From prevention to treatment to maintenance, we deliver a
+                best-in-class, consistent mental healthcare experience to
+                members across the globe.
+              </p>
+            </details>
+            <details name="faqs">
+              <summary>
+                <div className="title">
+                  <span>01</span> Evidence-based approach
+                </div>
+              </summary>
+              <p>Contenido oculto con texto de ejemplo.</p>
+            </details>
+            <details name="faqs">
+              <summary>
+                <div className="title">
+                  <span>01</span> Care anytime, anywhere
+                </div>
+              </summary>
+              <p>Contenido oculto con texto de ejemplo.</p>
+            </details>
+            <details name="faqs">
+              <summary>
+                <div className="title">
+                  <span>01</span> Leading the way in high-quality care
+                </div>
+              </summary>
+              <p>Contenido oculto con texto de ejemplo.</p>
+            </details>
+            <details name="faqs">
+              <summary>
+                <div className="title">
+                  <span>01</span> Team-based holistic care
+                </div>
+              </summary>
+              <p>Contenido oculto con texto de ejemplo.</p>
+            </details>
+            <details name="faqs">
+              <summary>
+                <div className="title">
+                  <span>01</span> Drive cultural change
+                </div>
+              </summary>
+              <p>Contenido oculto con texto de ejemplo.</p>
+            </details>
+          </div>
+        </div>
+      </section>
+
+      <section className="faq">
+        <div className="title-wraper">
+          <h2>Try contact today</h2>
+          <p>
+            No download, no install, no waiting.
+            <br />
+            Start creating instantly.
+          </p>
+        </div>
+        <button>Get started for free</button>
+      </section>
+      <footer>
+        <div className="links">
+          <p>© Hojas 2025. All rights reserved</p>
+          <ul>
+            <li>
+              <a href="">
+                <img src="/icons/vector.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/vector-1.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/vector-2.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/vector-3.svg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/icons/vector-4.svg" alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="black"></div>
+      </footer>
+    </>
+  );
+}
+
+export default Hero;
