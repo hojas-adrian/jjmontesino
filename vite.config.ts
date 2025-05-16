0b39f5b2659ea11f55961dfb5adc563c51e41f7b
Adrián Hojas Moré <hojas_adrian@MacBook-Air-de-Adrian.local>
2025-05-16 11:00:05 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/jjmontesino/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+  },
 });
